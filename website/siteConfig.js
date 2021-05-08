89f22deb187bb7764e52e6b6fe56d0a509dd2f61
Soumya Khanna <soumya.khanna99@gmail.com>
2021-05-08 10:19:04 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Soumya Khanna', // Title for your website.
     tagline: 'Aspring developer',
-    profilePhoto: './SK.jpg',
+    profilePhoto: '../assets/SK.jpg',
     url: 'https://soumyakhanna.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
