f352d469bde764bdeb7d978f6285a107b0976485
Soumya Khanna <soumya.khanna99@gmail.com>
2021-05-08 10:12:16 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,9 +13,9 @@ const users = [];
 
 const siteConfig = {
     title: 'Soumya Khanna', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
-    profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    tagline: 'Aspring developer',
+    profilePhoto: '',
+    url: 'https://soumyakhanna.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'soumyakhanna',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -39,7 +39,7 @@ const siteConfig = {
     // If you have users set above, you add it here:
     users,
 
-    favicon: 'img/favicon.ico',
+    favicon: '',
 
     /* Colors for website */
     colors: {
